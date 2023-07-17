f6245da7d2103738c4302b7c9e6fce45839572d3
tanwayne71 <106575332+tanwayne71@users.noreply.github.com>
2023-07-17 13:18:07 +0800

    Initial Pages Commit

---
+++ webdevA2/js/script.js
@@ -0,0 +1,47 @@
+//alert("script.js running");
+//target all elements to save to constants
+const page1btn=document.querySelector("#page1btn");
+const page2btn=document.querySelector("#page2btn");
+const page3btn=document.querySelector("#page3btn");
+var allpages=document.querySelectorAll(".page");
+//select all subtopic pages
+
+console.log(allpages);
+
+hideall();
+function hideall(){ //function to hide all pages
+    for(let onepage of allpages){ //go through all subtopic pages
+        onepage.style.display="none"; //hide it
+    }
+}
+
+function show(pgno){ //function to show selected page no
+    hideall();
+    //select the page based on the parameter passed in
+    let onepage=document.querySelector("#page"+pgno);
+    //show the page
+    onepage.style.display="block";
+}
+
+/*Listen for clicks on the buttons, assign anonymous
+eventhandler functions to call show function*/
+
+//() means calls immediately, use {} to make it call on click 
+page1btn.addEventListener("click", function () { //annoyomus fucntion
+    show(1);
+});
+page2btn.addEventListener("click", function () {
+    show(2);
+});
+page3btn.addEventListener("click", function () {
+    show(3);
+});
+
+/*for hamMenu */
+const hamBtn=document.querySelector("#hamIcon");
+hamBtn.addEventListener("click",toggleMenus);
+const menuItemsList=document.querySelector("nav ul");
+
+function toggleMenus(){ /*open and close menu*/
+    menuItemsList.classList.toggle("menuShow");
+}//can optimize using toggle class with css transition
